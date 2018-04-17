7ef44eb5678947d4694c1adab6be63f50bf2c0a3
意 钟 <zy19901021xy@outlook.com>
2018-04-17 19:45:11 +0800

    excel 不能退出

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog3 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonEmpty = new System.Windows.Forms.Button();
+            this.buttonReplace = new System.Windows.Forms.Button();
+            this.buttonNew = new System.Windows.Forms.Button();
+            this.openFileDialog4 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonAddFile = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "需要处理的文件";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "输入文本";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(292, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "查找";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(583, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "替换";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(180, 273);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(247, 21);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(495, 272);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(234, 21);
+            this.textBox2.TabIndex = 8;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // openFileDialog3
+            // 
+            this.openFileDialog3.FileName = "openFileDialog3";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(180, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(549, 188);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "文件";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 549;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(29, 99);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(87, 29);
+            this.buttonDelete.TabIndex = 10;
+            this.buttonDelete.Text = "删除文件";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonEmpty
+            // 
+            this.buttonEmpty.Location = new System.Drawing.Point(29, 152);
+            this.buttonEmpty.Name = "buttonEmpty";
+            this.buttonEmpty.Size = new System.Drawing.Size(87, 30);
+            this.buttonEmpty.TabIndex = 11;
+            this.buttonEmpty.Text = "清空文件";
+            this.buttonEmpty.UseVisualStyleBackColor = true;
+            this.buttonEmpty.Click += new System.EventHandler(this.buttonEmpty_Click);
+            // 
+            // buttonReplace
+            // 
+            this.buttonReplace.Location = new System.Drawing.Point(180, 502);
+            this.buttonReplace.Name = "buttonReplace";
+            this.buttonReplace.Size = new System.Drawing.Size(247, 37);
+            this.buttonReplace.TabIndex = 12;
+            this.buttonReplace.Text = "替换";
+            this.buttonReplace.UseVisualStyleBackColor = true;
+            this.buttonReplace.Click += new System.EventHandler(this.buttonReplace_Click);
+            // 
+            // buttonNew
+            // 
+            this.buttonNew.Location = new System.Drawing.Point(495, 502);
+            this.buttonNew.Name = "buttonNew";
+            this.buttonNew.Size = new System.Drawing.Size(234, 37);
+            this.buttonNew.TabIndex = 13;
+            this.buttonNew.Text = "生成";
+            this.buttonNew.UseVisualStyleBackColor = true;
+            this.buttonNew.Click += new System.EventHandler(this.buttonNew_Click);
+            // 
+            // openFileDialog4
+            // 
+            this.openFileDialog4.FileName = "openFileDialog4";
+            // 
+            // buttonAddFile
+            // 
+            this.buttonAddFile.Location = new System.Drawing.Point(29, 49);
+            this.buttonAddFile.Name = "buttonAddFile";
+            this.buttonAddFile.Size = new System.Drawing.Size(87, 29);
+            this.buttonAddFile.TabIndex = 3;
+            this.buttonAddFile.Text = "添加文件";
+            this.buttonAddFile.UseVisualStyleBackColor = true;
+            this.buttonAddFile.Click += new System.EventHandler(this.buttonAddFile_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Excel生成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 551);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonNew);
+            this.Controls.Add(this.buttonReplace);
+            this.Controls.Add(this.buttonEmpty);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonAddFile);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.o);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonEmpty;
+        private System.Windows.Forms.Button buttonReplace;
+        private System.Windows.Forms.Button buttonNew;
+        private System.Windows.Forms.OpenFileDialog openFileDialog4;
+        private System.Windows.Forms.Button buttonAddFile;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string filename;
+        string ExcelPath;
+
+        private void buttonAddFile_Click(object sender, EventArgs e)
+        {
+            
+            //openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "文本文件|*.txt";
+            this.openFileDialog1.ShowDialog();
+
+            //  openFileDialog1.FileOk()
+        }
+
+        private void o(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            
+            filename = openFileDialog1.FileName;
+            this.dataGridView1.Rows.Add(filename);
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count==0)
+            {
+                MessageBox.Show("请选择需要删除的文件!");
+            }
+
+            else
+            {
+                foreach (DataGridViewRow DataGridViewRowOne in dataGridView1.SelectedRows)  //集合之间怎么操作? one collection in another collection
+                                                                                            // DataGridViewRowOne.SetValues("");
+                    this.dataGridView1.Rows.Remove(DataGridViewRowOne);
+            }
+        }
+
+        private void buttonEmpty_Click(object sender, EventArgs e)
+        {
+
+
+            //for (int i=0;i< dataGridView1.Rows.Count;i++)
+            //{
+            //    //int j = dataGridView1.Rows.Count;
+            //    //dataGridView1.Rows.RemoveAt(0);
+                
+            //}
+
+            dataGridView1.Rows.Clear();
+
+        }
+
+        private void buttonReplace_Click(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text is null)
+            {
+                MessageBox.Show("请输入需要替换的字符!");
+            }
+            else
+            {
+                string SearchStr = this.textBox1.Text;
+            }
+
+            if (this.textBox2.Text is null)
+            {
+                MessageBox.Show("请输入用于替换的字符!");
+            }
+            else
+            {
+                string ReplaceStr = this.textBox2.Text;
+            }
+
+
+            ArrayList ListFileNames = new ArrayList();
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+            {
+                ListFileNames.Add(dataGridView1.Rows[i].Cells[0].Value.ToString());
+            }
+
+           
+
+            TxtReplace(ListFileNames, textBox1.Text, textBox2.Text);
+
+
+        }
+
+        public void TxtReplace(ArrayList StrArraylist, string SearchStr, string RepalceStr)
+        {
+            foreach (string  item in StrArraylist)
+            {
+                string temp = "";
+
+                FileStream fs1 = new FileStream(item.ToString(), FileMode.Open,FileAccess.Read);
+                StreamReader sr1 = new StreamReader(fs1);
+                temp=sr1.ReadToEnd();
+                sr1.Close();
+                fs1.Close();
+
+
+                temp = temp.Replace(SearchStr, RepalceStr);
+
+                FileStream fs2 = new FileStream(item.ToString(), FileMode.Open, FileAccess.Write);
+                StreamWriter sw2 = new StreamWriter(fs2);
+                sw2.WriteLine(temp);
+                sw2.Close();
+                fs2.Close();
+
+                MessageBox.Show("替换完成!");
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.Filter = "Exlce文件|*.xls|Exlce文件|*.xlsx";
+            this.openFileDialog2.ShowDialog();
+
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+              ExcelPath = this.openFileDialog2.FileName;
+
+        }
+
+        private void buttonNew_Click(object sender, EventArgs e)
+        {
+
+            ArrayList SearchList = new ArrayList();
+            ArrayList ReplaceList = new ArrayList();
+
+            object missing = System.Reflection.Missing.Value;
+
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+
+            excel.Visible = false; excel.UserControl = true;
+            // 以只读的形式打开EXCEL文件
+            Workbook wb = excel.Application.Workbooks.Open(ExcelPath, missing, true, missing, missing, missing,
+             missing, missing, missing, false, missing, missing, missing, missing, missing);
+            //取得第一个工作薄
+            Worksheet ws = (Worksheet)wb.Worksheets[1];
+            //取得总记录行数   (包括标题列)
+            int rowsint = ws.UsedRange.Cells.Rows.Count; //得到行数
+            //int columnsint = mySheet.UsedRange.Cells.Columns.Count;//得到列数
+            for (int i = 1; i <= rowsint; i++)
+            {
+                string tempSearch = ((Range)ws.Cells[i, 1]).Text;
+                SearchList.Add(tempSearch);
+                string tempReplace = ((Range)ws.Cells[i, 2]).Text;
+                ReplaceList.Add(tempReplace);
+            }
+
+            ArrayList ListFileNames = new ArrayList();
+            for (int i = 0; i <= dataGridView1.ColumnCount; i++)
+            {
+                ListFileNames.Add(dataGridView1.Rows[i].Cells[0].Value.ToString());
+            }
+
+            for (int i = 0; i < ListFileNames.Count; i++)
+            {
+                TxtMultyReplace(ListFileNames[i].ToString(), SearchList, ReplaceList);
+                ws.Cells[i, 3] = "转换完成!";
+            }
+
+            ws = null;
+            wb = null;
+            excel.Quit();
+            
+
+        }
+
+        public void TxtMultyReplace(string path,ArrayList SerchArrayList,ArrayList ReplacearrayList)
+        {
+            FileStream fileStreamNewTxt = new FileStream(path.Replace(".txt", "_copy.txt"), FileMode.Create, FileAccess.Write);
+            FileStream fileStreamOldTxt = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+            StreamWriter sw = new StreamWriter(fileStreamNewTxt);
+            StreamReader sr1 = new StreamReader(fileStreamOldTxt);
+            string OldTxt = sr1.ReadToEnd();
+
+            if (SerchArrayList.Count!=ReplacearrayList.Count)
+            {
+                MessageBox.Show("搜索字符和替换字符数量不相等!");
+                 
+            }
+
+            int cou = SerchArrayList.Count;
+
+            for (int i = 0; i < cou; i++)
+            {
+                
+                string NewTxt = OldTxt.Replace(SerchArrayList[i].ToString(), ReplacearrayList[i].ToString());
+                sw.WriteLine(NewTxt);
+            }
+
+            sr1.Close();
+            sw.Close();
+            fileStreamOldTxt.Close();
+            fileStreamNewTxt.Close();
+
+        }
+        
+}  
+}
